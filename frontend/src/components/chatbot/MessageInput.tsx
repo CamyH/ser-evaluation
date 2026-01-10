a271a7277fc0c7f23142b0c8271da097d20b8fd5
CamyH <cameronhunt88@hotmail.com>
2026-01-10 21:16:36 +0000

    style: formatting

--- frontend/src/components/chatbot/MessageInput.tsx
+++ frontend/src/components/chatbot/MessageInput.tsx
@@ -1,26 +1,31 @@
 import styles from '@/components/chatbot/styles/messageInput.module.css'
-import {InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput} from "@/components/ui/input-group.tsx";
-import {useState} from "react";
-import {SendHorizontal} from "lucide-react";
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupButton,
+    InputGroupInput,
+} from '@/components/ui/input-group.tsx'
+import { useState } from 'react'
+import { SendHorizontal } from 'lucide-react'
 
 export const MessageInput = () => {
-    const [isSent, setIsSent] = useState<boolean>(false);
+    const [isSent, setIsSent] = useState<boolean>(false)
     return (
         <section className={styles.inputContainer}>
             <InputGroup>
-                <InputGroupInput type='text' placeholder='Send a message...'/>
-                <InputGroupAddon align='inline-end'>
+                <InputGroupInput type="text" placeholder="Send a message..." />
+                <InputGroupAddon align="inline-end">
                     <InputGroupButton
-                        aria-label='Submit'
-                        type='submit'
-                        size='icon-xs'
+                        aria-label="Submit"
+                        type="submit"
+                        size="icon-xs"
                         onClick={() => setIsSent(!isSent)}
-                        disabled={isSent}>{
-                        <SendHorizontal type='submit'/>
-                    }
+                        disabled={isSent}
+                    >
+                        {<SendHorizontal type="submit" />}
                     </InputGroupButton>
                 </InputGroupAddon>
             </InputGroup>
         </section>
-    );
-};
+    )
+}
