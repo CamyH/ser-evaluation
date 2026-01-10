9bd14d81ef5eef284895d54e4d7ed8946872c312
CamyH <cameronhunt88@hotmail.com>
2026-01-10 21:16:08 +0000

    style: formatting

--- frontend/src/components/chatbot/Message.tsx
+++ frontend/src/components/chatbot/Message.tsx
@@ -1,12 +1,13 @@
-import {Item, ItemContent} from "@/components/ui/item.tsx";
-import type {BaseMessage} from "@/components/chatbot/types/message.type.ts";
+import { Item, ItemContent } from '@/components/ui/item.tsx'
+import type { BaseMessage } from '@/components/chatbot/types/message.type.ts'
+import styles from '@/components/chatbot/styles/message.module.css'
 
 type MessagePropType = BaseMessage
 
-export const Message = ({content}: MessagePropType) => {
+export const Message = ({ content }: MessagePropType) => {
     return (
-        <Item variant='outline' size='sm'>
+        <Item variant="outline" size="sm" className={styles.message}>
             <ItemContent>{content}</ItemContent>
         </Item>
-    );
-};
+    )
+}
