0593b8f64ce2001fbf77e8c7bdb172ca7f22e939
CamyH <cameronhunt88@hotmail.com>
2026-01-10 22:27:19 +0000

    feat: add initial audio service setup

---
+++ backend/AudioProcessingService/Interfaces/Services/IAudioService.cs
@@ -0,0 +1,11 @@
+using AudioProcessingService.Models;
+
+namespace AudioProcessingService.Interfaces.Services;
+
+public interface IAudioService
+{
+    /// <summary>
+    /// Normalize Audio using pre-processing steps:
+    /// </summary>
+    AudioData ProcessAudioAsync();
+}
---
+++ backend/AudioProcessingService/Services/AudioService.cs
@@ -0,0 +1,13 @@
+using AudioProcessingService.Interfaces.Services;
+using AudioProcessingService.Models;
+
+namespace AudioProcessingService.Services;
+
+public class AudioService : IAudioService
+{
+    /// <inheritdoc />
+    public AudioData ProcessAudioAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
