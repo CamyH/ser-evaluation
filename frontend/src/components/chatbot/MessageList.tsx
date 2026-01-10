9005dcd68f7484887cfd60f19ce7fdf23989790b
CamyH <cameronhunt88@hotmail.com>
2026-01-10 21:15:54 +0000

    refactor: improve semantic html styling

--- frontend/src/components/chatbot/MessageList.tsx
+++ frontend/src/components/chatbot/MessageList.tsx
@@ -1,17 +1,17 @@
-import type {Message as MessageType} from "@/components/chatbot/types/message.type.ts";
-import {Message} from "@/components/chatbot/Message.tsx";
+import type { Message as MessageType } from '@/components/chatbot/types/message.type.ts'
+import { Message } from '@/components/chatbot/Message.tsx'
 import styles from '@/components/chatbot/styles/messageList.module.css'
 
 type MessageListPropType = {
-    messages: MessageType[],
+    messages: MessageType[]
 }
 
-export const MessageList = ({messages}: MessageListPropType) => {
+export const MessageList = ({ messages }: MessageListPropType) => {
     return (
-        <section className={styles.messageContainer}>
+        <li className={styles.messageList}>
             {messages.map((message, index) => {
-                return <Message content={message.content} key={index}/>
+                return <Message content={message.content} key={index} />
             })}
-        </section>
-    );
-};
+        </li>
+    )
+}
