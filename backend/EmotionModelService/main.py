c732ae3d6ce5a6fd729a95e87c982e608dcffda0
CamyH <cameronhunt88@hotmail.com>
2026-01-24 14:32:37 +0000

    feat: add audio endpoint

--- backend/EmotionModelService/main.py
+++ backend/EmotionModelService/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
+from librosa import load
+from io import BytesIO
+from services.emotion_classification import process_audio
 
 app = FastAPI()
-
 
 @app.get("/api/v1/")
 async def health():
     return {"Healthy"}
 
-@app.post("/api/v1/audio")
-async def audio():
-    return None
+@app.post("/api/v1/audio/")
+async def audio(model: str, normalized_audio: bytes = File(...)) -> str:
+    loaded_audio, _ = load(BytesIO(normalized_audio), sr=16000, mono=True)
+
+    prediction = process_audio(model, loaded_audio)
+
+    return prediction
