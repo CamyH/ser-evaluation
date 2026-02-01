3098fecc9cad84b39fad834265ce8361f5362d78
CamyH <cameronhunt88@hotmail.com>
2026-02-01 17:02:50 +0000

    fix: exception handler not setup correctly

--- backend/AudioProcessingService/AudioProcessingService.Api/Program.cs
+++ backend/AudioProcessingService/AudioProcessingService.Api/Program.cs
@@ -1,12 +1,21 @@
+using System.Net;
 using AudioProcessingService.Api.Extensions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Controllers
+builder.Services.AddControllers();
 
 // Add Services
 builder.Services.AddServices();
 
 // Add Pipelines
 builder.Services.AddPipelines();
+
+// Health Check
+builder.Services.AddHealthChecks()
+    .AddCheck("healthy", () => HealthCheckResult.Healthy());
 
 var app = builder.Build();
 
@@ -15,6 +24,17 @@ if (app.Environment.IsDevelopment()) { }
 
 app.UseHttpsRedirection();
 
-app.UseExceptionHandler();
+app.UseExceptionHandler(error =>
+{
+    error.Run(async context =>
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        await context.Response.WriteAsync("An unexpected error occurred.");
+    });
+});
+
+app.MapHealthChecks("/api/v1/health");
+
+app.MapControllers();
 
 app.Run();
